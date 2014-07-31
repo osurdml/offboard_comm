5485c32c6ae38160855abc68663ca91bc23b2e7e
Kyle Cesare <kcesare@gmail.com>
2014-07-31 13:10:23 -0700

    Initial move_base work.

--- src/offboard_comm.cpp
+++ src/offboard_comm.cpp
@@ -1,24 +1,34 @@
 #include <offboard_comm.h>
 
 #include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <move_base_msgs/MoveBaseGoal.h>
 
 #include <setpoint_transmitter.h>
+
+void actionCallback(const move_base_msgs::MoveBaseGoalConstPtr &action) {
+  ROS_INFO("GOT ACTION");
+}
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "qex_gs");
 	ros::NodeHandle nh("~");
 
-	SetpointTransmitter *transmitter = new SetpointTransmitter();
+	// SetpointTransmitter *transmitter = new SetpointTransmitter();
 
-	ros::Subscriber qex_gs_tf_proc = nh.subscribe("/tf", 10, &SetpointTransmitter::tfProcCallback, transmitter);
-	ros::Subscriber qex_gs_target_proc = nh.subscribe("/target", 10, &SetpointTransmitter::targetProcCallback, transmitter);
+	// ros::Subscriber qex_gs_tf_proc = nh.subscribe("/tf", 10, &SetpointTransmitter::tfProcCallback, transmitter);
+	// ros::Subscriber qex_gs_target_proc = nh.subscribe("/target", 10, &SetpointTransmitter::targetProcCallback, transmitter);
+
+        actionlib::SimpleActionServer<move_base_msgs::MoveBaseAction> as(nh, "/move_base", &actionCallback, false);
+        as.start();
 
 	ros::Rate rate(10); // 5Hz
 	while(ros::ok()) {
-		ROS_INFO("Sending...");
+		// ROS_INFO("Sending...");
 
 		// TODO: Reset SP after some timeout on the TF topic
-		transmitter->transmit();
+		// transmitter->transmit();
 
 		ros::spinOnce();
 		rate.sleep();
